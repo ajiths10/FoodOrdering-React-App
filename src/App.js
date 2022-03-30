e9cc096cf4393271e30a4e52252164be9217e48a
Nuclear One <ajithsnair10@gmail.com>
2022-03-30 10:18:38 +0530

    Food Ordering React App StartUp

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <div>
+      <div className="Header">
+      <h1 className="h1txt">ReactMeals</h1>
+        <div className='button-Background'>
+          <div className= "divtxt"> Your Cart  <button className = "counts"> 0</button> </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
